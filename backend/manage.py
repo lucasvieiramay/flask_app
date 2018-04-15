2ae628d2c70496a87a26f1af46257586c35cdf4b
lucasmay <lucas.may@contentools.com>
2018-04-15 19:38:29 -0300

    FIXING migrate manager, and adding a column called deleted to do the soft delete

--- backend/manage.py
+++ backend/manage.py
@@ -1,15 +1,11 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from local_settings import POSTGRES
 
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
-%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+from app import app
+from persons.models import db
 
-db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
 
 manager = Manager(app)
