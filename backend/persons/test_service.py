136ad483622525a2cb392d95bf6be87126ae8cf8
lucasmay <lucas.may@contentools.com>
2018-04-16 13:38:24 -0300

    testing get_all_objects_function

--- backend/persons/test_service.py
+++ backend/persons/test_service.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import patch
 from persons.services import PersonService
+from persons.models import Person
 
 
 class TestPersonService(unittest.TestCase):
@@ -9,7 +10,7 @@ class TestPersonService(unittest.TestCase):
     def test_filter_objects_by_name(self, model_mock):
         """
         This function receive a dictionary
-        With the filter parameter
+        With the filter parameter name
         """
         model_mock.return_value = []
         # Test case with name parameter
@@ -21,10 +22,54 @@ class TestPersonService(unittest.TestCase):
     def test_filter_objects_by_email(self, model_mock):
         """
         This function receive a dictionary
-        With the filter parameter
+        With the filter parameter email
         """
         model_mock.return_value = []
-        # Test case with name parameter
+        # Test case with email parameter
         filter_field = {'email': 'test@gmail.com'}
         PersonService().filter_objects(filter_field)
         assert model_mock.called
+
+    @patch('persons.services.Person.query_by_birth_date')
+    def test_filter_objects_by_birth_date(self, model_mock):
+        """
+        This function receive a dictionary
+        With the filter parameter birth_date
+        """
+        model_mock.return_value = []
+        # Test case with email parameter
+        filter_field = {'birth_date': '20/07/1994'}
+        PersonService().filter_objects(filter_field)
+        assert model_mock.called
+
+    @patch('persons.services.Person.query_by_doc_id')
+    def test_filter_objects_by_doc_id(self, model_mock):
+        """
+        This function receive a dictionary
+        With the filter parameter doc_id
+        """
+        model_mock.return_value = []
+        # Test case with email parameter
+        filter_field = {'doc_id': '00012345601'}
+        PersonService().filter_objects(filter_field)
+        assert model_mock.called
+
+    @patch('persons.services.PersonSchema')
+    @patch('persons.services.Person')
+    def test_get_all_objects(self, model_mock, serialzier_mock):
+        """
+        Should return all objects serialized
+        """
+        params = {
+            "email": "teste@gmail.com",
+            "birth_date": "1994-07-20",
+            "doc_id": "00012345601",
+            "name": "Lucas May",
+            "id": 5
+        }
+        test_obj = Person(**params)
+        model_mock.query.all.return_value = [test_obj]
+        serialzier_mock.dump.return_value = [params]
+        PersonService().get_all_objects()
+        assert model_mock.query.all.called
+        assert serialzier_mock.called
