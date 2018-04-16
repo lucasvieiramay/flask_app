1fa067f02bf79c537398659b1fc4884242c10581
lucasmay <lucas.may@contentools.com>
2018-04-16 16:18:50 -0300

    unittest for person/list route

--- backend/test_app.py
+++ backend/test_app.py
@@ -1,5 +1,6 @@
 import unittest
 from app import app
+from unittest.mock import patch
 
 
 class TestApiCalls(unittest.TestCase):
@@ -7,6 +8,16 @@ class TestApiCalls(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def test_person_list(self):
+    @patch('persons.services.PersonService.get_all_objects')
+    def test_person_list(self, mock_service):
+        mock_service.return_value = []
         response = self.app.get('/persons/list')
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == 200
+        assert mock_service.called
+
+    @patch('persons.services.PersonService.filter_objects')
+    def test_person_list_with_parameters(self, mock_service):
+        mock_service.return_value = []
+        response = self.app.get('/persons/list?name=Lucas')
+        assert response.status_code == 200
+        assert mock_service.called
